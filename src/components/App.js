0927c794fb4d2ff80046d987d07ae3d941c3f3f8
EYoung75 <eyoung75.ey@gmail.com>
2019-04-03 11:46:51 -0600

    semantic ui styling for list being rendered in SongList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import '../App.css';
 import { selectSong } from "../actions"
+import SongList from "./songList"
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList/>
+        </div>
+      </div>
     </div>
   )
 }
