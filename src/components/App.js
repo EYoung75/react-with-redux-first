305ee79da5eae45109acb52b372233ccb161c060
EYoung75 <eyoung75.ey@gmail.com>
2019-04-03 11:07:40 -0600

    installed redux and react-redux dependencies. Created folders for reducers and actions. Created and exported a selectSong action and songReducer / selectedSongReducer

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import { selectSong } from "../actions"
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default App;
